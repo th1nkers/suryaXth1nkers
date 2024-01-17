1eb568c5d5f438c406c4679a68afc6d3ec9f0f1d
th1nkers <suryasingh11112003@gmail.com>
2024-01-17 19:37:41 +0530

    vercel3

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,7 +1,7 @@
 const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
-const Page = require('../models/Page');
+const Page = require('../models/page');
 
 const HttpError = require('../models/http-error');
 
