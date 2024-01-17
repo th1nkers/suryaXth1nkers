4a38e86780edc016decee906ffbc75dada829971
th1nkers <suryasingh11112003@gmail.com>
2024-01-17 19:32:58 +0530

    vercel2

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 require('dotenv').config();
 
 const usersRoutes = require('./routes/users-routes');
@@ -32,21 +31,6 @@ app.use((req, res, next) => {
     const error = new HttpError('Could not find this route.', 404);
     throw error;
 })
-
-app.use((error, req, res, next) => {
-
-    if (req.file) {
-        fs.unlink(req.file.path, err => {
-            console.log(err);
-        });
-    };
-
-    if (res.headerSent) return next(error);
-
-    res.status(error.code || 500);
-    res.json({ message: error.message || 'An unknown error occurred!' });
-});
-
 
 mongoose.
     connect(MONGO_URL)
